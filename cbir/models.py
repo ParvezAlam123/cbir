697492dd18f6e73ff647a86a4f92007b61e7eafa
luke <luke.k.vincent@outlook.com>
2016-04-04 18:43:11 +0100

    changed field types to binary

--- cbir/models.py
+++ cbir/models.py
@@ -4,11 +4,10 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     file = models.ImageField(upload_to='images/', null=True)
-    bgrHist = models.TextField(blank=True, null=True)
-    hsvHist = models.TextField(blank=True, null=True)
+    bgrHist = models.BinaryField(blank=True, null=True)
+    hsvHist = models.BinaryField(blank=True, null=True)
+    texture = models.BinaryField(blank=True, null=True)
     lbpHist = models.BinaryField(blank=True, null=True)
-    dissimilarity = models.TextField(blank=True, null=True)
-    correlation = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
 
